3efc89d2b69414435556753933fa28c2ea22b3fd
bluesun147 <bluesun147@naver.com>
2024-03-11 19:48:41 +0900

    [Fix] gradle 설정

--- src/main/java/com/example/demo/dto/OstFeignClient.java
+++ src/main/java/com/example/demo/dto/OstFeignClient.java
@@ -1,4 +1,4 @@
-package com.haechan.content.global.feign;
+package com.example.demo.dto;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
